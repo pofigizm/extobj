074e600cf4439dbb4040526b9f1dcfef92306bde
Denis Iogansen <pofigizm@gmail.com>
2014-10-11 17:11:24 +0400

    init all

---
+++ src/extobj.js
@@ -0,0 +1,17 @@
+/*
+ * extobj
+ * https://github.com/Pofigizm/extobj
+ *
+ * Copyright (c) 2014 Denis
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
